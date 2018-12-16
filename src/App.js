74dc0cb97c7ca72e78a36f67cd14411d716cdcfa
Rahul Kumar <rforrahul83@gmail.com>
2018-12-16 00:13:52 -0500

    using props in component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import Ninjas from './ninjas';
 
 class App extends Component {
-  
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
   render() {
     return (
       <div className="App">
           <h1>My First react app</h1>
           <p>Welcome :)</p>
-          <Ninjas name="Ryu" age="25" belt="black" />
+          <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
--- src/ninjas.js
+++ src/ninjas.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react';
 class Ninjas extends Component {
     render() {
         console.log(this.props);
-        const { name, age, belt } = this.props;
+        const { ninjas } = this.props;
+        const ninjasList = ninjas.map(({ name, age, belt, id }) => {
+            return (
+                <div className="ninja" key={id}>
+                    <div>Name: {name}</div>
+                    <div>Age: {age}</div>
+                    <div>Belt: {belt}</div>
+                </div>
+            )
+        });
         return (
-            <div className="ninja">
-                <div>Name: {name}</div>
-                <div>Age: {age}</div>
-                <div>Belt: {belt}</div> 
+            <div className="ninja-list">
+                { ninjasList }
             </div>
         )
     }
