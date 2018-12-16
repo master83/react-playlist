f941ff27746fa9f506e0cccbf0f7f1ff040c0d97
Rahul Kumar <rforrahul83@gmail.com>
2018-12-16 00:16:27 -0500

    change component to stateless/ui component

--- src/ninjas.js
+++ src/ninjas.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-    render() {
-        console.log(this.props);
-        const { ninjas } = this.props;
-        const ninjasList = ninjas.map(({ name, age, belt, id }) => {
-            return (
-                <div className="ninja" key={id}>
-                    <div>Name: {name}</div>
-                    <div>Age: {age}</div>
-                    <div>Belt: {belt}</div>
-                </div>
-            )
-        });
+const Ninjas = (props) => {
+    const { ninjas } = props;
+    const ninjasList = ninjas.map(({ name, age, belt, id }) => {
         return (
-            <div className="ninja-list">
-                { ninjasList }
+            <div className="ninja" key={id}>
+                <div>Name: {name}</div>
+                <div>Age: {age}</div>
+                <div>Belt: {belt}</div>
             </div>
         )
-    }
+    });
+    return (
+        <div className="ninja-list">
+            { ninjasList }
+        </div>
+    )
 }
 
 export default Ninjas;
